cd20005f82f2a45bd642649810b4856760eca7b9
XxSharkDesertxX <xx.alirahjo.xx@gmail.com>
2022-07-22 22:05:09 +0430

    responsive navbar

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="container_header">
+    <div>
+        <div class="container_header">
 
 
 
-        <ul class="mnue_container" >
+        <ul class="mnue_container">
             <li>
                 <nuxt-link to="/" exact  class="nxt_link_header">Home</nuxt-link>
             </li>
@@ -18,16 +19,54 @@
             </li>
         </ul>
 
+
+       
+
         <div class="logo_header">
             <img src="../../static/image_logo/Logo2.png" alt="">
         </div>
 
+        </div>
+                <v-icon class="icon_mnue_bar" @click="responsive">
+                    {{istrue ? 'mdi-menu' : 'mdi-minus-thick'}}
+                </v-icon>
+          <ul class="mnue_container_sml">
+            <li>
+                <nuxt-link to="/" exact  class="nxt_link_header">Home</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/Content" class="nxt_link_header">Content</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="Login" class="nxt_link_header">Login</nuxt-link>
+            </li>
+            <li>
+                <nuxt-link to="/About" class="nxt_link_header">About</nuxt-link>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 export default {
-    name:"Header"
+    name:"Header",
+    data() {
+        return {
+            istrue:true
+        }
+    },
+    methods: {
+        responsive(){
+            let data = document.querySelector('.mnue_container_sml')
+            if (this.istrue) {
+                data.style.width="40%"
+                this.istrue = false
+            } else {
+                data.style.width="0%"
+                this.istrue = true
+            }
+        }
+    },
 }
 </script>
 
@@ -75,7 +114,42 @@ export default {
         transition: all .6s;
     }
     
-    
+    .mnue_container_sml{
+       display: none;
+    }
+    .icon_mnue_bar{
+        display: none;
+    }
 
-    
+    @media screen and (max-width:700px) {
+       .container_header{
+        display: none;
+       }
+       .mnue_container_sml{
+        display: flex; justify-content: space-around; align-items: center; flex-direction: column;
+        background: rgba(26, 26, 26,.8);
+        position: fixed;
+        width: 0 ;
+        height: 100%;
+        z-index: 200;
+        transition: all .6s ease;
+        overflow: hidden;
+    }
+    .mnue_container_sml li{
+        list-style: none;
+    }
+    .icon_mnue_bar{
+        position: fixed;
+        right: 2rem;
+        top: 2rem;
+        z-index: 200;
+        display: block;
+        cursor: pointer;
+        font-size: 2rem;
+    }
+    .nxt_link_header{
+        color: #fff;
+        font-size: 1.6rem;
+    }
+    }
 </style>
